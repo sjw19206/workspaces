b8197c0ec437c1a9e0fbf630beefa8ba9dcd4201
Sharon Wilson <105675912+sjw19206@users.noreply.github.com>
2023-06-30 22:15:15 +0100

    Update DefineVariables.java

--- java_8/basics/src/org/sjw19206/datatypes/DefineVariables.java
+++ java_8/basics/src/org/sjw19206/datatypes/DefineVariables.java
@@ -4,6 +4,8 @@ public class DefineVariables {
 
 	boolean bVar; // 1 bit
 
+	char cVar;
+
 	byte btVar; // 1 byte
 	short sVar; // 2 bytes
 	int iVar; // 4 bytes
@@ -15,14 +17,24 @@ public class DefineVariables {
 	public DefineVariables() {
 
 		bVar = true; // 1 bit
-		iVar = 10; // 4 bytes
+
+		cVar = 'A';
+
+		btVar = 105;
+		sVar = 200;
+		iVar = 1230; // 4 bytes
+		lVar = 15000l;
+
+		fVar = 234.678f;
+		dVar = 34.56d;
 	}
 
 	public void displayBytes() {
 
-		System.out.println("bVar  :: " + bVar);		
-		System.out.println("btVar :: " + Byte.BYTES);
-		System.out.println("iVar  :: " + Integer.BYTES);
+		System.out.println("byte Var   :: " + Byte.BYTES);
+		System.out.println("short Var  :: " + Short.BYTES);
+		System.out.println("int Var    :: " + Integer.BYTES);
+		System.out.println("long Var   :: " + Long.BYTES);
 	}
 
 	public static void main(String[] args) {
