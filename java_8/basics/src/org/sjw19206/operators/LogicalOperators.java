4cb608875cc82826c7f1e878647c6b8cbd616074
Sharon Wilson <105675912+sjw19206@users.noreply.github.com>
2023-07-01 23:47:36 +0100

    Update LogicalOperators.java

--- java_8/basics/src/org/sjw19206/operators/LogicalOperators.java
+++ java_8/basics/src/org/sjw19206/operators/LogicalOperators.java
@@ -2,9 +2,44 @@ package org.sjw19206.operators;
 
 public class LogicalOperators {
 
+	/*
+	 * Logical AND (&&) - does not check second condition, if FIRST one is FALSE
+	 * 
+	 * Bitwise AND (&) - checks both conditions
+	 * 
+	 * Logical OR (||) - does not check second condition, if FIRST one is TRUE
+	 * 
+	 * Bitwise OR (|) - checks both conditions
+	 */
+
 	public static void main(String[] args) {
-		
-		
+
+		int a = 10, b = 12, c = 20;
+
+		System.out.println(a + " " + b + " " + c);
+
+		/*
+		 * second condition won't be checked and value of 'a' remains same
+		 */
+		System.out.println(a > b && ++a == c);
+
+		System.out.println(a + " " + b + " " + c);
+
+		/*
+		 * both the conditions will be checked and value of 'a' getting changed
+		 */
+		System.out.println(a > b & ++a == c);
+
+		System.out.println(a + " " + b + " " + c);
+
+		System.out.println(++a >= b || ++a == c);
+
+		System.out.println(a + " " + b + " " + c);
+
+		System.out.println(++a >= b | ++a == c);
+
+		System.out.println(a + " " + b + " " + c);
+
 	}
 
 }
